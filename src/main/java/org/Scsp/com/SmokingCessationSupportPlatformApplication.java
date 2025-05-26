5b988f4db0b63f8f35c8645a76b61cb82e1697aa
Hung <lequanghung471@gmail.com>
2025-05-26 17:55:49 +0700

    Initial commit

---
+++ src/main/java/org/Scsp/com/SmokingCessationSupportPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.Scsp.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokingCessationSupportPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmokingCessationSupportPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/Scsp/com/SmokingCessationSupportPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Scsp.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokingCessationSupportPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
