d1f51fd9092681bbeadcd5fe715c2c6d521bb304
PCuong <45710344+PCuong101@users.noreply.github.com>
2025-06-18 08:32:33 +0700

    update booking

--- src/main/java/org/Scsp/com/repository/SlotRepository.java
+++ src/main/java/org/Scsp/com/repository/SlotRepository.java
@@ -4,8 +4,8 @@ import org.Scsp.com.model.Slot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public interface SlotRepository extends JpaRepository<Slot, Long> {
-    Slot findBySlotID(Long slotID);
 }
