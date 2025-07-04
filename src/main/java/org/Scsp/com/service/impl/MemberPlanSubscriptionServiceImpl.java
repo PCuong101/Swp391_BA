ab5e2e48d15a340d92bddc5d319364cc0a52143a
bactxse180308 <bactxse180308@fpt.edu.vn>
2025-07-04 14:29:06 +0700

    admin set schedule

--- src/main/java/org/Scsp/com/service/impl/MemberPlanSubscriptionServiceImpl.java
+++ src/main/java/org/Scsp/com/service/impl/MemberPlanSubscriptionServiceImpl.java
@@ -1,58 +1,58 @@
-package org.Scsp.com.service.impl;
+    package org.Scsp.com.service.impl;
 
-import lombok.RequiredArgsConstructor;
-import org.Scsp.com.Enum.MemberPlanSubscriptionStatus;
-import org.Scsp.com.Enum.Role;
-import org.Scsp.com.model.MemberPlan;
-import org.Scsp.com.model.MemberPlanSubscription;
-import org.Scsp.com.model.MemberPlanSubscriptionId;
-import org.Scsp.com.model.User;
-import org.Scsp.com.repository.MemberPlanRepository;
-import org.Scsp.com.repository.MemberPlanSubscriptionRepository;
-import org.Scsp.com.service.MemberPlanSubscriptionService;
-import org.springframework.stereotype.Service;
+    import lombok.RequiredArgsConstructor;
+    import org.Scsp.com.Enum.MemberPlanSubscriptionStatus;
+    import org.Scsp.com.Enum.Role;
+    import org.Scsp.com.model.MemberPlan;
+    import org.Scsp.com.model.MemberPlanSubscription;
+    import org.Scsp.com.model.MemberPlanSubscriptionId;
+    import org.Scsp.com.model.User;
+    import org.Scsp.com.repository.MemberPlanRepository;
+    import org.Scsp.com.repository.MemberPlanSubscriptionRepository;
+    import org.Scsp.com.service.MemberPlanSubscriptionService;
+    import org.springframework.stereotype.Service;
 
-import java.util.List;
+    import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class MemberPlanSubscriptionServiceImpl implements MemberPlanSubscriptionService {
+    @Service
+    @RequiredArgsConstructor
+    public class MemberPlanSubscriptionServiceImpl implements MemberPlanSubscriptionService {
 
-    private final MemberPlanSubscriptionRepository memberPlanSubscriptionRepository;
-    private final MemberPlanRepository memberPlanRepository;
-    private final UsersServiceImpl usersService;
-    @Override
-    public MemberPlanSubscription subscribe(Long memberId, Long planId) {
-        User user = usersService.getUserById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("User not found with id: " + memberId));
-        MemberPlan memberPlan = memberPlanRepository.findById(planId)
-                .orElseThrow(() -> new IllegalArgumentException("Plan not found with id: " + planId));
-        MemberPlanSubscriptionId memberPlanSubscriptionId = new MemberPlanSubscriptionId(user.getUserId(),memberPlan.getPlanID());
-        user.setRole(Role.MEMBER_VIP1);
+        private final MemberPlanSubscriptionRepository memberPlanSubscriptionRepository;
+        private final MemberPlanRepository memberPlanRepository;
+        private final UsersServiceImpl usersService;
+        @Override
+        public MemberPlanSubscription subscribe(Long memberId, Long planId) {
+            User user = usersService.getUserById(memberId)
+                    .orElseThrow(() -> new IllegalArgumentException("User not found with id: " + memberId));
+            MemberPlan memberPlan = memberPlanRepository.findById(planId)
+                    .orElseThrow(() -> new IllegalArgumentException("Plan not found with id: " + planId));
+            MemberPlanSubscriptionId memberPlanSubscriptionId = new MemberPlanSubscriptionId(user.getUserId(),memberPlan.getPlanID());
+            user.setRole(Role.MEMBER_VIP1);
 
-        MemberPlanSubscription subscription = MemberPlanSubscription.builder()
-                .id(memberPlanSubscriptionId)
-                .user(user)
-                .status(MemberPlanSubscriptionStatus.ACTIVE)
-                .plan(memberPlan)
-                .build();
+            MemberPlanSubscription subscription = MemberPlanSubscription.builder()
+                    .id(memberPlanSubscriptionId)
+                    .user(user)
+                    .status(MemberPlanSubscriptionStatus.ACTIVE)
+                    .plan(memberPlan)
+                    .build();
 
-        return memberPlanSubscriptionRepository.save(subscription);
+            return memberPlanSubscriptionRepository.save(subscription);
+        }
+
+        @Override
+        public List<MemberPlanSubscription> getSubscriptions(Long memberId) {
+            return  memberPlanSubscriptionRepository.findById_UserID(memberId);
+        }
+
+        @Override
+        public void cancelSubscription(Long userId, Long memberId) {
+            MemberPlanSubscriptionId subscriptionId = new MemberPlanSubscriptionId(userId, memberId);
+            MemberPlanSubscription sub = memberPlanSubscriptionRepository.findById(subscriptionId)
+                    .orElseThrow(() -> new RuntimeException("Not found"));
+
+            sub.setStatus(MemberPlanSubscriptionStatus.CANCELLED);
+
+            memberPlanSubscriptionRepository.save(sub);
+        }
     }
-
-    @Override
-    public List<MemberPlanSubscription> getSubscriptions(Long memberId) {
-        return  memberPlanSubscriptionRepository.findById_UserID(memberId);
-    }
-
-    @Override
-    public void cancelSubscription(Long userId, Long memberId) {
-        MemberPlanSubscriptionId subscriptionId = new MemberPlanSubscriptionId(userId, memberId);
-        MemberPlanSubscription sub = memberPlanSubscriptionRepository.findById(subscriptionId)
-                .orElseThrow(() -> new RuntimeException("Not found"));
-
-        sub.setStatus(MemberPlanSubscriptionStatus.CANCELLED);
-
-        memberPlanSubscriptionRepository.save(sub);
-    }
-}
