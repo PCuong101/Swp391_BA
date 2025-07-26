c77d52d526cf1d27d466a96bd0c69c37055a801d
thienddse180316 <thienddse180316@fpt.edu.vn>
2025-07-26 10:02:20 +0700

    update password

--- src/main/java/org/Scsp/com/dto/UserUpdateDTO.java
+++ src/main/java/org/Scsp/com/dto/UserUpdateDTO.java
@@ -8,5 +8,6 @@ public class UserUpdateDTO {
     // Chỉ cho phép cập nhật những trường này
     private String name;
     private String email;
-    private String profilePicture; // Frontend sẽ gửi URL ảnh ở đây
+    private String profilePicture;
+    private String password;// Frontend sẽ gửi URL ảnh ở đây
 }
